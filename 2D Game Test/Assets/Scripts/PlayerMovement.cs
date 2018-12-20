25ee635f8cb3f3f290059b3d4912330f263acac9
Jacob Stenson <jacob@nosnets.co.uk>
2018-12-20 00:49:23 +0000

    Original Commit
    Game in this state is a simple 2D game.
    Player can walk around on several platforms.
    Player can also shoot a bullet.
    
    Adding all the assets.

---
+++ 2D Game Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 25f;
+
+    float horizontalMove = 0f;
+    bool doJump = false;
+    bool doCrouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            doJump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            doCrouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            doCrouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, doCrouch, doJump);
+        doJump = false;
+    }
+}
